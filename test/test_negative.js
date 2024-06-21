7a46cacb4b8a29b5ab37bb4e0526234b09110447
Sofiia Rostkovych <s.rostkovych@gmail.com>
2024-06-21 18:03:04 +0300

    added 'before'

--- test/test_negative.js
+++ test/test_negative.js
@@ -1,5 +1,6 @@
 const {Builder, By, Key} = require("selenium-webdriver");
 const assert = require("assert");
+const webdriver = require("selenium-webdriver");
 
 let invalidLoginInfo = [       //data of the possible invalid users
     { username: "", password: "SuperSecretPassword!" },
@@ -10,12 +11,12 @@ let invalidLoginInfo = [       //data of the possible invalid users
     { username: "TOMSMITH", password: "SuperSecretPassword!" }
 ];
 describe("Negative tests", ()=>{
+
     //test for the login of the invalid users
     invalidLoginInfo.forEach((invalidUser)=>{
         //calling negative_test function for each user of the invalidLoginInfo array
         it(`Unsuccessful login test: username = '${invalidUser.username}', password = '${invalidUser.password}'`, async ()=>{
-        let driver = await new Builder().forBrowser("chrome").build();
-        try {
+            let driver =  await new Builder().forBrowser("chrome").build();
             await driver.get("https://the-internet.herokuapp.com/login");
             await driver.findElement(By.id("username")).sendKeys(invalidUser.username);
             await driver.findElement(By.id("password")).sendKeys(invalidUser.password);
@@ -27,13 +28,11 @@ describe("Negative tests", ()=>{
                 assert.equal(msg.includes("Your password is invalid!"), true);//checking if the error is displayed for incorrect password
             }
             assert.equal(await driver.getCurrentUrl(), "https://the-internet.herokuapp.com/login"); //checking if user remains on the login page
-        } catch (err) {
-            console.log(err);
-        } finally {
             await driver.quit();
-        }
     });
+
 });
+
 })
 
 
--- test/test_positive.js
+++ test/test_positive.js
@@ -2,6 +2,10 @@ const {Builder, By, Key} = require("selenium-webdriver");
 const assert = require("assert");
 
 describe("Positive test", function(){
+    let driver;
+    beforeEach( async function (){
+        await new Builder().forBrowser("chrome").build();
+    });
     //test for the login of the valid user "tomsmith" with password "SuperSecretPassword!"
     it("Successful login", async function(){
         let driver = await new Builder().forBrowser("chrome").build();
@@ -11,7 +15,6 @@ describe("Positive test", function(){
         let msg = await driver.findElement(By.xpath("//*[@id=\"flash\"]")).getText();
         assert.equal(msg.includes("You logged into a secure area!"), true);//checking the success message
         assert.equal(await driver.getCurrentUrl(), "https://the-internet.herokuapp.com/secure");//checking if the secure area is opened
-
         await driver.quit();
     });
 });

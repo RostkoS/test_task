4a0f504e9ee8b8c13abba7289b953253879f8dd4
Sofiia Rostkovych <s.rostkovych@gmail.com>
2024-06-21 18:49:33 +0300

    fixes

--- test/test_positive.js
+++ test/test_positive.js
@@ -4,11 +4,11 @@ const assert = require("assert");
 describe("Positive test", function(){
     let driver;
     beforeEach( async function (){
-        await new Builder().forBrowser("chrome").build();
+        driver = await new Builder().forBrowser("chrome").build();
     });
     //test for the login of the valid user "tomsmith" with password "SuperSecretPassword!"
     it("Successful login", async function(){
-        let driver = await new Builder().forBrowser("chrome").build();
+
         await driver.get("https://the-internet.herokuapp.com/login");
         await driver.findElement(By.id("username")).sendKeys("tomsmith");
         await driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!", Key.RETURN);
